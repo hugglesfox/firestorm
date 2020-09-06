2b11de9693a19ec559d44db14e9e363894a9adde
Hayley Hughes <hayley@foxes.systems>
2020-09-06 19:12:14 +1000

    Add comments to the todo example

--- examples/todo.cpp
+++ examples/todo.cpp
@@ -13,13 +13,19 @@ struct Todo {
   }
 };
 
+// To do database
 vector<Todo> todos = {};
+
+// Gets updated on each request depending on the data sent.
+// This might not always be a valid todo.
 Todo current_todo;
 
+// Middleware to attempt to parse the current todo from a request
 Outcome parse_todo(http_request request) {
-  write_line(request_body(request));
+  // This is gonna be annoying and log warnings unless it's a post request.
   json body = json_from_string(request_body(request));
 
+  // Update current_todo
   if (json_has_key(body, "text")) {
     current_todo.text = json_read_string(body, "text");
   }
@@ -27,9 +33,11 @@ Outcome parse_todo(http_request request) {
   if (json_has_key(body, "is_done")) {
     current_todo.is_done = json_read_bool(body, "is_done");
   }
+
   return Outcome::Success;
 }
 
+// A GET route for /todos to list all the todos
 Response list_todos(UriVars _) {
   vector<json> todos_json;
   for (Todo todo : todos) {
@@ -42,19 +50,22 @@ Response list_todos(UriVars _) {
   return json_data(response, HTTP_STATUS_OK);
 }
 
+// A GET route for /todos/<id> to list a single todo
 Response get_todo(UriVars vars) {
   if (!is_number(vars["id"])) {
     throw HTTP_STATUS_BAD_REQUEST;
   }
 
+  int i = convert_to_integer(vars["id"]);
+
   try {
-    int i = convert_to_integer(vars["id"]);
     return json_data(todos.at(i).to_json(), HTTP_STATUS_OK);
   } catch (...) {
     throw HTTP_STATUS_NOT_FOUND;
   }
 }
 
+// A POST route for /todos to create a new todo
 Response create_todo(UriVars _) {
   if (current_todo.text != "") {
     todos.push_back(current_todo);
@@ -64,18 +75,22 @@ Response create_todo(UriVars _) {
   throw HTTP_STATUS_BAD_REQUEST;
 }
 
+// A DELETE route for /todos/<id> to delete a todo
 Response delete_todo(UriVars vars) {
   if (!is_number(vars["id"])) {
     throw HTTP_STATUS_BAD_REQUEST;
   }
 
+  int i = convert_to_integer(vars["id"]);
+
   try {
-    int i = convert_to_integer(vars["id"]);
     todos.erase(todos.begin() + i);
-    return status(HTTP_STATUS_NO_CONTENT);
   } catch (...) {
-    throw HTTP_STATUS_NO_CONTENT;
   }
+
+  // Return 203 no matter whether it failed or not as if the todo your trying to
+  // delete already doesn't exist then it was still a successful deletion.
+  return status(HTTP_STATUS_NO_CONTENT);
 }
 
 int main() {
