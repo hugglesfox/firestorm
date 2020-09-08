9bdb9ab594f53b194407af760add4df85cd05642
Hayley Hughes <hayley@foxes.systems>
2020-09-08 17:09:25 +1000

    Restructure route

--- routing.h
+++ routing.h
@@ -2,24 +2,16 @@
 #define FIRESTORM_ROUTING
 
 #include "include/splashkit/splashkit.h"
-#include <unordered_map>
+
+#include "middlewares/parse_uri_vars.h"
 #include "response.h"
 #include "utils.h"
 
-using UriVars = std::unordered_map<string, string>;
-using RouteFn = Response (*)(UriVars path);
-
-struct Route {
-
-  string uri;
-  http_method method;
-  RouteFn fn;
-
-  bool matches(http_request request);
-  Response route(http_request request);
-  UriVars uri_vars(http_request request);
-  UriVars path_vars(http_request request);
-  UriVars arg_vars(http_request request);
+class Route {
+public:
+  vector<MiddleWare<Route>> middlewares = {};
+  UriVars vars;
+  virtual Response route() { throw HTTP_STATUS_NOT_FOUND; }
 };
 
 #endif
