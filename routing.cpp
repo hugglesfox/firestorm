94eb22198b4ef6ad9fdbf77a5895a38f330e7610
Hayley Hughes <hayley@foxes.systems>
2020-09-10 21:40:44 +1000

    Update Route

--- routing.cpp
+++ routing.cpp
@@ -2,10 +2,3 @@
 #include "include/splashkit/splashkit.h"
 
 using namespace std;
-
-Outcome<Response> Route::handle(http_request request) {
-  for (MiddleWare<Route> middleware : middlewares()) {
-    Outcome<Route> outcome = middleware.handle(request);
-
-  }
-}
--- routing.h
+++ routing.h
@@ -2,18 +2,33 @@
 #define FIRESTORM_ROUTING
 
 #include "include/splashkit/splashkit.h"
+#include <memory>
 
 #include "middlewares/parse_uri_vars.h"
 #include "response.h"
 
 class Route {
+private:
+  http_request request;
+
 protected:
-  virtual vector<MiddleWare<Route>> middlewares() { return {}; };
+  template <typename R>
+  Outcome use(R &route, vector<MiddleWare<R> *> middlewares) {
+    for (MiddleWare<R> *middleware : middlewares) {
+      if (middleware->handle(route, request) == Outcome::Failure) {
+        return Outcome::Failure;
+      }
+    }
+
+    return Outcome::Success;
+  }
 
 public:
   UriVars vars;
-  Outcome<Response> handle(http_request request);
-  virtual Outcome<Response> route() { throw HTTP_STATUS_NOT_FOUND; }
+
+  void set_request(http_request request) { request = request; }
+  virtual Outcome middlewares() { return Outcome::Failure; };
+  virtual Response route() { throw HTTP_STATUS_NOT_FOUND; }
 };
 
 #endif
