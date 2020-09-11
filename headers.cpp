fff1033b83c48de8c836d994b05f9a64bdc19050
Hayley Hughes <hayley@foxes.systems>
2020-09-11 19:11:46 +1000

    Remove debug code

--- headers.cpp
+++ headers.cpp
@@ -3,7 +3,6 @@
 Headers headers(http_request request) {
   Headers result;
   for (string header : request_headers(request)) {
-    write_line(header);
     vector<string> parts = split_at(header, ':');
     if (parts.size() == 2) {
       result[parts[0]] = result[parts[1]];
