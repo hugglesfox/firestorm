b11349dad7c28ddc3e2318c393e69e958c790584
Hayley Hughes <hayley@foxes.systems>
2020-09-24 17:51:12 +1000

    Add utils unit tests

---
+++ test/utils/test_string_splitting.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+#include "../src/utils.cpp"
+
+TEST_CASE("test split at") {
+  REQUIRE(split_at("foo bar", ' ') == vector<string> {"foo", "bar"});
+  REQUIRE(split_at("foobar", ' ') == vector<string> {"foobar"});
+  REQUIRE(split_at("", 'a') == vector<string> {""});
+  REQUIRE(split_at("foobar", 'f') == vector<string> {"", "oobar"});
+  REQUIRE(split_at("foobar", 'r') == vector<string> {"fooba", ""});
+}
+
+TEST_CASE("test split at first") {
+  REQUIRE(split_at_first("foobar", 'o') == vector<string> {"f", "obar"});
+  REQUIRE(split_at_first("foo b ar", ' ') == vector<string> {"foo", "b ar"});
+}
