03f3e5dc7c573a7b27beaa4b9768b042069331df
Hayley Hughes <hayley@foxes.systems>
2020-09-26 22:30:00 +1000

    Improve utils unittests

--- test/utils/test_string_splitting.cpp
+++ test/utils/test_string_splitting.cpp
@@ -5,11 +5,14 @@ TEST_CASE("test split at") {
   REQUIRE(split_at("foo bar", ' ') == vector<string> {"foo", "bar"});
   REQUIRE(split_at("foobar", ' ') == vector<string> {"foobar"});
   REQUIRE(split_at("", 'a') == vector<string> {""});
+  REQUIRE(split_at("foobar", 'o') == vector<string> {"f", "", "bar"});
   REQUIRE(split_at("foobar", 'f') == vector<string> {"", "oobar"});
   REQUIRE(split_at("foobar", 'r') == vector<string> {"fooba", ""});
 }
 
 TEST_CASE("test split at first") {
   REQUIRE(split_at_first("foobar", 'o') == vector<string> {"f", "obar"});
+  REQUIRE(split_at_first("foobar", 'f') == vector<string> {"", "oobar"});
+  REQUIRE(split_at_first("foobar", 'r') == vector<string> {"fooba", ""});
   REQUIRE(split_at_first("foo b ar", ' ') == vector<string> {"foo", "b ar"});
 }
