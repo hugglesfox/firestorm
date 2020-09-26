8c7057b9d7f5673ab8dbfef46226257f440b8f1c
Hayley Hughes <hayley@foxes.systems>
2020-09-26 21:13:01 +1000

    Add custom error example

---
+++ examples/custom_error.cpp
@@ -0,0 +1,26 @@
+#include "firestorm.h"
+
+class Hello : public Route {
+public:
+  UriArgs uri_args;
+
+  Outcome middlewares() {
+    return MiddleWares<Hello>()
+        .add(new Router<Hello>(HTTP_GET_METHOD,
+                               {"/hello", "/hello/<name>?<age>"}))
+        .outcome(*this);
+  }
+
+  Response response() {
+    return plain("Hello " + uri_args["name"] + " aged " + uri_args["age"]);
+  }
+};
+
+Response not_found() {
+  return plain("Nothing to see here...");
+}
+
+int main() {
+  FireStorm().add_route(new Hello()).not_found(not_found).ignite();
+  return 0;
+}
