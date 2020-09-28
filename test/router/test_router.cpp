775597a5b9d688d414322b0342ed702aeeb7871e
Hayley Hughes <hayley@foxes.systems>
2020-09-28 20:05:28 +1000

    Rework router tests

--- test/router/test_router.cpp
+++ test/router/test_router.cpp
@@ -1,2 +1,22 @@
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "firestorm_mock.h"
+
+TEST_CASE("test uri vars parsing") {
+  http_request request = MockRequest()
+                            .uri("/animals/dog?color=black")
+                            .construct();
+
+  REQUIRE(uri_vars(request, "/animals/<animal>?<color>")["color"] == "black");
+  REQUIRE(uri_vars(request, "/animals/<animal>?<color>")["animal"] == "dog");
+  REQUIRE(uri_vars(request, "/").size() == 0);
+}
+
+TEST_CASE("test uri matches") {
+  http_request request = MockRequest()
+                            .uri("/animals/dog?color=black")
+                            .construct();
+
+  REQUIRE(uri_matches(request, "/animals/<animal>/?<color>"));
+  REQUIRE(!uri_matches(request, "/animals/animal>?<color"));
+  REQUIRE(!uri_matches(request, "/a"));
+}
