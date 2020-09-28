144557ad3c98cf003269190318e09f3c681bee02
Hayley Hughes <hayley@foxes.systems>
2020-09-28 20:17:07 +1000

    Rework form tests

--- test/form/test_form.cpp
+++ test/form/test_form.cpp
@@ -1,2 +1,9 @@
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "form.h"
+
+TEST_CASE("test parse form") {
+  string form = "hello=world&foo=bar";
+
+  REQUIRE(parse_form(form)["hello"] == "world");
+  REQUIRE(parse_form(form)["foo"] == "bar");
+}
