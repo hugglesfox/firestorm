7a23ec9e688f10f1664e1154352d27f3f424e2e3
Hayley Hughes <hayley@foxes.systems>
2020-09-28 20:27:20 +1000

    Add test for split at first

--- test/utils/test_utils.cpp
+++ test/utils/test_utils.cpp
@@ -4,3 +4,7 @@
 TEST_CASE("test split at") {
   REQUIRE(split_at("foo bar", ' ') == vector<string> {"foo", "bar"});
 }
+
+TEST_CASE("test split at first") {
+  REQUIRE(split_at_first("foobar", 'o') == vector<string> {"f", "obar"});
+}
