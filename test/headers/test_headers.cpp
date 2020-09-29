608bfa9236f455aac7c0aa3a8460974bb67c7a47
Hayley Hughes <hayley@foxes.systems>
2020-09-29 12:10:52 +1000

    Rework header tests

--- test/headers/test_headers.cpp
+++ test/headers/test_headers.cpp
@@ -3,16 +3,18 @@
 #include "firestorm_mock.h"
 
 TEST_CASE("test headers") {
-  auto request = MockRequest().add_header("Host: 127.0.0.1").construct();
-  REQUIRE(headers(&request)["Host"] == "127.0.0.1");
-}
-
-TEST_CASE("test cookies") {
-  auto request = MockRequest()
+   auto request = MockRequest()
+                    .add_header("Host: 127.0.0.1")
                     .add_cookie(Cookie("foo", "bar"))
                     .add_cookie(Cookie("hello", "world"))
                     .construct();
 
-  REQUIRE(cookies(&request)["foo"] == "bar");
+  SECTION("test header parsing") {
+    REQUIRE(headers(&request)["Host"] == "127.0.0.1");
+  }
+
+  SECTION("test cookie parsing") {
+    REQUIRE(cookies(&request)["foo"] == "bar");
+  }
 }
 
