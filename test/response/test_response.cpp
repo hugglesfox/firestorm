6cf1bfb54b53c4eceb2f7ca36f21875731c72429
Hayley Hughes <hayley@foxes.systems>
2020-09-29 13:15:48 +1000

    Add response tests

---
+++ test/response/test_response.cpp
@@ -0,0 +1,33 @@
+#include "catch.hpp"
+#include "response.h"
+
+TEST_CASE("test response") {
+  Response response;
+  response.content_type = "text/plain";
+  response.status = HTTP_STATUS_OK;
+
+  SECTION("test status") {
+    REQUIRE(status(HTTP_STATUS_OK) == response);
+  }
+
+  SECTION("test plain") {
+    response.body = "Hello World\n";
+    REQUIRE(plain("Hello World") == response);
+  }
+
+  SECTION("test json") {
+    response.content_type = "application/json";
+    response.body = "{\n    \"hello\": \"world\"\n}\n";
+
+    json body = create_json("{\"hello\": \"world\"}");
+    REQUIRE(json_data(body) == response);
+  }
+
+  SECTION("test redirect") {
+    response.content_type = "";
+    response.status = HTTP_STATUS_SEE_OTHER;
+    response.headers = {"Location: example.com"};
+    REQUIRE(redirect("example.com") == response);
+  }
+}
+
